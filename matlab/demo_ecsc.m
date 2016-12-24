114061b63874ffc0c52352b1528544efcbdf145e
Tran Minh Quan <quantm@unist.ac.kr>
2016-12-24 17:30:33 +0900

    Implemented custom Fourier transform with higher resolution, zero padding

--- matlab/demo_ecsc.m
+++ matlab/demo_ecsc.m
@@ -9,14 +9,14 @@ dictSize  = 64; % Number of atoms in the dictionary
 dataSize  = 6;  % Number of images in the entire dataset
 
 %% Define the inner tensor shape
-atomInner = [15,   15,  1]; % For grayscale atom, it is 15x15x1
-dataInner = [256, 256,  1]; % For grayscale image, it is 256x256x1
+atomInner = [15,   15,  1, 1]; % For grayscale atom, it is dimy * dimx * dimz * 1 
+dataInner = [256, 256,  1, 1]; % For grayscale image, it is dimy * dimx * dimz * 3 
 
 %% Define the outer tensor shape
 atomOuter = [atomInner, dictSize,        1]; 	% original dictionary size, before padding
-dictOuter = [dataInner, dictSize,        1]; 	% dimy * dimx * dimz * dimk * 1, zeropadding
-dataOuter = [dataInner,        1, dataSize]; 	% dimy * dimx * dimz *    1 * dimn
-blobOuter = [dataInner, dictSize, dataSize]; 	% dimy * dimx * dimz * dimk * 1, zeropadding
+dictOuter = [dataInner, dictSize,        1]; 	% dimy * dimx * dimz * dimc * dimk * 1, zeropadding
+dataOuter = [dataInner,        1, dataSize]; 	% dimy * dimx * dimz * dimc *    1 * dimn
+blobOuter = [dataInner, dictSize, dataSize]; 	% dimy * dimx * dimz * dimc * dimk * 1, zeropadding
 
 %% Define a bunch of parameters, if we run on the graph base, needs to wrap by a class of parameters
 Lambda   = 0.1;
@@ -42,37 +42,47 @@ fixed_d  = false;
 fixed_x  = false; % Control parameters of whether we want to optimize the dictionary of the sparse code
 
 %% Initialization, Preallocate the layer placeholder, 5D tensorshape
-Si   = complex(zeros(dataOuter)); % dimy * dimx * dimz * 1 * dimn; data placeholder
-Sf   = complex(zeros(dataOuter)); % dimy * dimx * dimz * 1 * dimn; freq placeholder 
-
-%% PrecomputeFetch the image data to S and perform custom fourier transform
-% images should be normalized within the range [0, 1]
-S0   = images;
+Si   = complex(zeros(dataOuter)); % dimy * dimx * dimz * dimc * 1 * dimn; data placeholder
+Sf   = complex(zeros(dataOuter)); % dimy * dimx * dimz * dimc * 1 * dimn; freq placeholder 
 
 %% Initialize other related splitting variables for sparse codes
-Yi   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; current sparse code @ iteration i
-Yf   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; current sparse code in Fourier domain
-Yp   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; previous sparse code 
-Ui   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; dual variable U @ iteration i
+Yi   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; current sparse code @ iteration i
+Yf   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; current sparse code in Fourier domain
+Yp   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; previous sparse code 
+Ui   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; dual variable U @ iteration i
 
-Xi   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; current sparse code @ iteration i
-Xf   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; current sparse code in Fourier domain
-Xr   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; relaxed sparse code
-Uz   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimk * dimn; residual of X and Y, in Fourier domain
+Xi   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; current sparse code @ iteration i
+Xf   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; current sparse code in Fourier domain
+Xr   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; relaxed sparse code
+Uz   = complex(zeros(blobOuter)); % dimy  * dimx * dimz * dimc * dimk * dimn; residual of X and Y, in Fourier domain
 
 %% Initialize other related splitting vvariables for dictionary learning
-Gi   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; current dictionary @ iteration i
-Gf   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; current dictionary in Fourier domain
-Gp   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; previous dictionary 
-Hi   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; dual variable H @ iteration i
+Gi   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; current dictionary @ iteration i
+Gf   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; current dictionary in Fourier domain
+Gp   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; previous dictionary 
+Hi   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; dual variable H @ iteration i
 
-Di   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; current dictionary @ iteration i
-Df   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; current dictionary in Fourier domain
-Dr   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; relaxed dictionary 
-Hz   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimk * 1; residual of D and G, in Fourier domain
+Di   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; current dictionary @ iteration i
+Df   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; current dictionary in Fourier domain
+Dr   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; relaxed dictionary 
+Hz   = complex(zeros(dictOuter)); % dimy  * dimx * dimz * dimc * dimk * 1; residual of D and G, in Fourier domain
 
 % That's all for constructing the graph
 %%
+
+%% Fetch the image data to S and perform custom fourier transform
+% images should be normalized within the range [0, 1]
+S0   = images;
+
+%% Precompute the frequency
+Si   = complex(S0);
+Si   = reshape(Si, dataOuter);
+Sf   = cfft(Si);
+
+%% Launch the iteration process
+for i=1:Jmax
+
+end
 
 
 
---
+++ matlab/test_cfft.m
@@ -0,0 +1,13 @@
+% Test custom cfft;
+clc; clear all; close all;
+
+% Load the images
+oldshape = [512, 512, 1, 1, 1, 6];
+paddings = [64, 64, 0, 0, 0, 0];
+
+imgs = imreadtif('std.tif');
+imgs = single(imgs);
+
+imgs = reshape(imgs, oldshape);
+freq = cfft(imgs, [1, 2], paddings);
+imgs = cifft(freq, [1, 2, paddings]);
